25c5fe238bc0e2dfbc69422b158e16ef9266aa37
Oscar-Rosenvilde <oscarse@afk.no>
2024-10-28 14:16:01 +0100

    opprettet js-intro

---
+++ projects/js/index.js
@@ -0,0 +1,50 @@
+const tavle = 10;
+
+var tall = 10;
+
+let tall2 = 10;
+
+
+var elev = "Sara";
+
+var tekst = "Hei, " + elev + "!!!";
+
+
+
+function minFunksjon(){
+
+    console.log(5);
+    console.log(5);
+    console.log(5);
+    console.log(5);
+    console.log(5);
+    console.log(5);
+    console.log(5);
+    console.log(5);
+    console.log(5);
+    console.log(5);
+    console.log(5);
+
+}
+
+
+
+
+function nyFunksjon(hvaSomHelst){
+    
+    var ting = hvaSomHelst;
+    
+    console.log("Handleliste:");
+    console.log(ting);
+    
+    document.getElementById("talletMitt").innerHTML = 1000;
+}
+
+
+var teller = 0;
+
+function tellerFunksjon(){
+
+    teller = teller + 1;
+    document.getElementById("talletMitt").style.color 
+}
